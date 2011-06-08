68651725f17b67f1b2d9730ba35e7ce84a5fa552
Michal Taszycki <michal@applicake.com>
2011-06-08 12:41:03 +0200

    keep alignment when changing screens feature added

--- ShiftIt/DefaultShiftItActions.m
+++ ShiftIt/DefaultShiftItActions.m
@@ -20,11 +20,13 @@
 #import "DefaultShiftItActions.h"
 #import "FMTDefines.h"
 
+static const int MAX_DIFFERENCE = 50.0f;
 BOOL almostEqualRects(CGRect a, CGRect b) {
-  return fabsf(a.origin.x - b.origin.x) < 16.0f &&
-         fabsf(a.origin.y - b.origin.y) < 16.0f &&
-         fabsf(a.size.width - b.size.width) < 16.0f &&
-         fabsf(a.size.height - b.size.height) < 16.0f;
+  
+  return fabsf(a.origin.x - b.origin.x) < MAX_DIFFERENCE &&
+         fabsf(a.origin.y - b.origin.y) < MAX_DIFFERENCE &&
+         fabsf(a.size.width - b.size.width) < MAX_DIFFERENCE &&
+         fabsf(a.size.height - b.size.height) < MAX_DIFFERENCE;
 }
 
 NSPoint add(NSPoint a, NSPoint b) {
@@ -113,71 +115,126 @@ int findBottomCycleIndex(NSRect screen, NSRect window) {
   }
   return -1;
 }
+int findTopLeftCycleIndex(NSRect screen, NSRect window) {
+	NSRect r = screen;
+  for (int i = 0; i < ratioCount-1; i++) {
+    r.size.width = screen.size.width * ratios[i];
+    r.size.height = screen.size.height * ratios[i];
 
-NSRect computeRectangleWidthByCycleIndex(NSRect alreadyComputedRectangle, int cycleIndex) {
-  if (cycleIndex == -1) {
-    cycleIndex = 0
+    if (almostEqualRects(r,window)) {
+      return i;
+    }
   }
-  
+  return -1;
+}
+int findTopRightCycleIndex(NSRect screen, NSRect window) {
+	NSRect r = screen;
+  for (int i = 0; i < ratioCount-1; i++) {
+    r.size.width = screen.size.width * ratios[i];
+    r.size.height = screen.size.height * ratios[i];
+    r.origin.x = screen.origin.x + screen.size.width - r.size.width;
+
+    if (almostEqualRects(r,window)) {
+      return i;
+    }
+  }
+  return -1;
+}
+int findBottomLeftCycleIndex(NSRect screen, NSRect window) {
+	NSRect r = screen;
+  for (int i = 0; i < ratioCount-1; i++) {
+    r.size.height = screen.size.height * ratios[i];
+    r.size.width = screen.size.width * ratios[i];
+    r.origin.y = screen.origin.y + screen.size.height - r.size.height;
+
+    if (almostEqualRects(r,window)) {
+      return i;
+    }
+  }
+  return -1;
+}
+int findBottomRightCycleIndex(NSRect screen, NSRect window) {
+	NSRect r = screen;
+  for (int i = 0; i < ratioCount-1; i++) {
+    r.size.width = screen.size.width * ratios[i];
+    r.size.height = screen.size.height * ratios[i];
+    r.origin.x = screen.origin.x + screen.size.width - r.size.width;
+    r.origin.y = screen.origin.y + screen.size.height - r.size.height;
+
+    if (almostEqualRects(r,window)) {
+      return i;
+    }
+  }
+  return -1;
+}
+int nextCycleIndex(int cycleIndex) {
+  return (cycleIndex + 1) % ratioCount;
+}
+
+NSRect computeRectangleWidthByCycleIndex(NSRect screen, NSRect alreadyComputedRectangle, int cycleIndex) {
   alreadyComputedRectangle.size.width = screen.size.width * ratios[cycleIndex];
+  return alreadyComputedRectangle;
 }
-NSRect computeRectangleHeightByCycleIndex(NSRect alreadyComputedRectangle, int cycleIndex) {
-  if (cycleIndex == -1) {
-    cycleIndex = 0
-  }
-  
+NSRect computeRectangleHeightByCycleIndex(NSRect screen, NSRect alreadyComputedRectangle, int cycleIndex) {
   alreadyComputedRectangle.size.height = screen.size.height * ratios[cycleIndex];
+  return alreadyComputedRectangle;
 }
-NSRect adjustTop(NSRect alreadyComputedRectangle, int cycleIndex) {
-  r.origin.y = screen.origin.y + screen.size.height - alreadyComputedRectangle.size.height;
+NSRect adjustTop(NSRect screen, NSRect alreadyComputedRectangle) {
+  alreadyComputedRectangle.origin.y = screen.origin.y + screen.size.height - alreadyComputedRectangle.size.height;
+  return alreadyComputedRectangle;
 }
 NSRect adjustLeft(NSRect screen, NSRect alreadyComputedRectangle) {
-  r.origin.x = screen.origin.x + screen.size.width - alreadyComputedRectangle.size.width;
+  alreadyComputedRectangle.origin.x = screen.origin.x + screen.size.width - alreadyComputedRectangle.size.width;
+  return alreadyComputedRectangle;
 }
 
 NSRect moveLeftByCycleIndex(NSRect screen, int cycleIndex) {
-  return computeRectangleWidthByCycleIndex(screen, cycleIndex);
+  NSRect result = screen;
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
+  return result;
 }
 NSRect moveTopByCycleIndex(NSRect screen, int cycleIndex) {
-  return computeRectangleHeightByCycleIndex(screen, cycleIndex);
+  NSRect result = screen;
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
+  return result;
 }
 NSRect moveRightByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleWidthByCycleIndex(result, cycleIndex);
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
   result = adjustLeft(screen, result);
-  return result
+  return result;
 }
 NSRect moveBottomByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleHeightByCycleIndex(result, cycleIndex);
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
   result = adjustTop(screen, result);
   return result;
 }
 
 NSRect moveTopLeftByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleWidthByCycleIndex(result, cycleIndex);
-  result = computeRectangleHeightByCycleIndex(result, cycleIndex);
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
   return result;
 }
 NSRect moveTopRightByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleWidthByCycleIndex(result, cycleIndex);
-  result = computeRectangleHeightByCycleIndex(result, cycleIndex);
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
   result = adjustLeft(screen, result);
   return result;
 }
 NSRect moveBottomLeftByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleWidthByCycleIndex(result, cycleIndex);
-  result = computeRectangleHeightByCycleIndex(result, cycleIndex);
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
   result = adjustTop(screen, result);
   return result;
 }
 NSRect moveBottomRightByCycleIndex(NSRect screen, int cycleIndex) {
   NSRect result = screen;
-  result = computeRectangleWidthByCycleIndex(result, cycleIndex);
-  result = computeRectangleHeightByCycleIndex(result, cycleIndex);
+  result = computeRectangleWidthByCycleIndex(screen, result, cycleIndex);
+  result = computeRectangleHeightByCycleIndex(screen, result, cycleIndex);
   result = adjustLeft(screen, result);
   result = adjustTop(screen, result);
   return result;
@@ -188,43 +245,43 @@ NSRect moveBottomRightByCycleIndex(NSRect screen, int cycleIndex) {
 
 
 NSRect ShiftIt_Left(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findLeftCycleIndex(screen, window);
-  return moveLeftByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findLeftCycleIndex(screen, window);
+  return moveLeftByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_Right(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findRightCycleIndex(screen, window);
-  return moveRightByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findRightCycleIndex(screen, window);
+  return moveRightByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_Top(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findTopCycleIndex(screen, window);
-  return moveTopByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findTopCycleIndex(screen, window);
+  return moveTopByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_Bottom(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findBottomCycleIndex(screen, window);
-  return moveBottomByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findBottomCycleIndex(screen, window);
+  return moveBottomByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_TopLeft(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findTopLeftCycleIndex(screen, window);
-  return moveTopLeftByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findTopLeftCycleIndex(screen, window);
+  return moveTopLeftByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_TopRight(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findTopRightCycleIndex(screen, window);
-  return moveTopRightByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findTopRightCycleIndex(screen, window);
+  return moveTopRightByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_BottomLeft(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findBottomLeftCycleIndex(screen, window);
-  return moveBottomLeftCycleIndex(screen, cycleIndex);
+  int cycleIndex = findBottomLeftCycleIndex(screen, window);
+  return moveBottomLeftByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_BottomRight(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  cycleIndex = findBottomRightCycleIndex(screen, window);
-  return moveBottomRightByCycleIndex(screen, cycleIndex);
+  int cycleIndex = findBottomRightCycleIndex(screen, window);
+  return moveBottomRightByCycleIndex(screen, nextCycleIndex(cycleIndex));
 }
 
 NSRect ShiftIt_FullScreen(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
@@ -242,22 +299,31 @@ NSRect ShiftIt_Center(NSRect screen, NSRect window, NSRect screens[], int screen
 	return r;
 }
 
-NSRect changeScreen(NSRect screen, NSRect window, NSRect screens[], int screenCount, int delta) {
-  FMTDevLog(@"changeScreen screen=[%.02f %.02f %.02f %.02f]",
-            screen.origin.x,screen.origin.y,screen.size.width,screen.size.height);
-  
-  int screenIndex;
-
-  for (screenIndex = 0; screenIndex < screenCount; screenIndex++) {
+int findCurrentScreenIndex(NSRect screen, NSRect screens[], int screenCount) {
+  for (int screenIndex = 0; screenIndex < screenCount; screenIndex++) {
     FMTDevLog(@"changeScreen screens[%i]=[%.02f %.02f %.02f %.02f]", screenIndex,
               screens[screenIndex].origin.x,screens[screenIndex].origin.y,
               screens[screenIndex].size.width,screens[screenIndex].size.height);
 
     if (equalRects(screens[screenIndex],screen))
-      break;
+      return screenIndex;
   }
+  return -1;
+}
+
+int nextScreenIndex(int screenIndex, int screenCount) {
+  return (screenIndex + 1) % screenCount;
+}
+int previousScreenIndex(int screenIndex, int screenCount) {
+  return (screenIndex + screenCount - 1) % screenCount;
+}
+
+NSRect changeScreen(NSRect screen, NSRect window, NSRect screens[], int screenCount, int delta) {
+  FMTDevLog(@"changeScreen screen=[%.02f %.02f %.02f %.02f]",
+            screen.origin.x,screen.origin.y,screen.size.width,screen.size.height);
   
-  if (screenIndex == screenCount) {
+  int screenIndex = findCurrentScreenIndex(screen, screens, screenCount);
+  if (screenIndex == -1) {
     FMTDevLog(@"screen not found");
     return window;
   }
@@ -285,13 +351,45 @@ NSRect changeScreen(NSRect screen, NSRect window, NSRect screens[], int screenCo
   
   return window;
 }
+NSRect tryToKeepAlignment(NSRect previousScreen, NSRect newScreen, NSRect window, NSRect alreadyComputedRectangle) {
+  int cycleIndex = -1;
+  NSRect result = alreadyComputedRectangle;
+  if ( (cycleIndex = findLeftCycleIndex(previousScreen, window)) != -1) {
+    result = moveLeftByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findRightCycleIndex(previousScreen, window)) != -1) {
+    result = moveRightByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findTopCycleIndex(previousScreen, window)) != -1) {
+    result = moveTopByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findBottomCycleIndex(previousScreen, window)) != -1) {
+    result = moveBottomByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findTopLeftCycleIndex(previousScreen, window)) != -1) {
+    result = moveTopLeftByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findTopRightCycleIndex(previousScreen, window)) != -1) {
+    result = moveTopRightByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findBottomLeftCycleIndex(previousScreen, window)) != -1) {
+    result = moveBottomLeftByCycleIndex(newScreen, cycleIndex);
+  } else if ( (cycleIndex = findBottomRightCycleIndex(previousScreen, window)) != -1) {
+    result = moveBottomRightByCycleIndex(newScreen, cycleIndex);
+  } 
+  return result;
+}
 
 NSRect ShiftIt_PrevScreen(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  return changeScreen(screen,window,screens,screenCount,-1);  
+  NSRect screenChangeRectangle = changeScreen(screen,window,screens,screenCount,-1);  
+  NSRect result = screenChangeRectangle;
+  int currentScreenIndex = findCurrentScreenIndex(screen, screens, screenCount);
+  NSRect newScreen = screens[previousScreenIndex(currentScreenIndex, screenCount)];
+  result = tryToKeepAlignment(screen, newScreen, window, result);
+  return result;
 }
 
 NSRect ShiftIt_NextScreen(NSRect screen, NSRect window, NSRect screens[], int screenCount) {
-  return changeScreen(screen,window,screens,screenCount,1);  
+  NSRect screenChangeRectangle = changeScreen(screen,window,screens,screenCount,1);  
+  NSRect result = screenChangeRectangle;
+  int currentScreenIndex = findCurrentScreenIndex(screen, screens, screenCount);
+  NSRect newScreen = screens[nextScreenIndex(currentScreenIndex, screenCount)];
+  result = tryToKeepAlignment(screen, newScreen, window, result);
+  return result;
 }
 
 
